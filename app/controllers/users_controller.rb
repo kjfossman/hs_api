6038b02e201e2636410dfa7968c688f002b2ca09
Kyle <kjfossman@gmail.com>
2021-08-25 16:07:24 -0800

    Updated the users controller to first search users by email and if not found then create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   # POST /users
   def create
-    @user = User.new(user_params)
+    @user = User.find_by(email: user_params["email"]) || User.new(user_params)
 
     if @user.save
       render json: @user, status: :created, location: @user
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:username, :email, :display_name)
+      params.require(:user).permit(:name, :email, :imageUrl)
     end
 end
